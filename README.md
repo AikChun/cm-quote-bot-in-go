ec46bdc72f8e631f56005fc05bc8113496a59803
aikchun <iam@simaikchun.com>
2022-05-30 10:44:56 +0800

    Refactored code into a controller-service based architecture

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cm-quote-bot-in-go
+
+# How to build for deployment
+
+```bash
+GOOS=GOOS=linux GOARCH=amd64 go build main.go handlers.go
+```
--- main.go
+++ main.go
@@ -8,28 +8,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	telegrambot "github.com/AikChun/yagotb"
+	"github.com/aikchun/cm-quote-bot-in-go/pkg/quotecontroller"
+	"github.com/aikchun/cm-quote-bot-in-go/pkg/quoteservice"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-pg/pg/v10"
 	"github.com/joho/godotenv"
 )
-
-func CreateTelegramBot() *telegrambot.Bot {
-	bot, err := telegrambot.NewBot(os.Getenv("BOT_TOKEN"))
-
-	if err != nil {
-		panic(err)
-	}
-
-	bot.AddHandler("/echo", Echo)
-	bot.AddHandler("/randomquote", RandomQuote)
-	bot.AddHandler("/savequote", SaveQuote)
-	bot.AddHandler("/latestquote", LatestQuote)
-	bot.AddHandler("/crisis", RandomCrisisQuote)
-	bot.AddHandler("/help", Help)
-
-	return bot
-}
 
 func HandleRequest(ctx context.Context, u telegrambot.Update) {
 	HandleTelegramUpdate(&u)
@@ -54,7 +41,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleTelegramUpdate(u *telegrambot.Update) {
-	bot := CreateTelegramBot()
+	bot, err := telegrambot.NewBot(os.Getenv("BOT_TOKEN"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	quotedUserID := os.Getenv("QUOTED_USER_ID")
+
+	userID, err := strconv.ParseInt(quotedUserID, 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	userName := os.Getenv("QUOTED_USER_NAME")
+
+	url := os.Getenv("DATABASE_URL")
+	opt, err := pg.ParseURL(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db := pg.Connect(opt)
+
+	cmQuoteController := quotecontroller.QuoteController{
+		QS: &quoteservice.QuoteService{
+			DB:       db,
+			UserID:   userID,
+			UserName: userName,
+		},
+	}
+
+	bot.AddHandler("/echo", Echo)
+	bot.AddHandler("/randomquote", cmQuoteController.RandomQuote)
+	bot.AddHandler("/savequote", cmQuoteController.SaveQuote)
+	bot.AddHandler("/latestquote", cmQuoteController.LatestQuote)
+	bot.AddHandler("/crisis", cmQuoteController.RandomCrisisQuote)
+	bot.AddHandler("/help", Help)
+
 	bot.HandleUpdate(u)
 }
 
